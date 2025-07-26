324b9417f863b9c340ae136f060aae895914836c
YYUCHII <3541838033@qq.com>
2025-07-26 15:27:33 +0800

    框架搭建

--- build/02_TreeStructrue/cmake_install.cmake
+++ build/02_TreeStructrue/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/25804/Desktop/data structure/data-structure---c
+# Install script for directory: C:/Users/25804/Desktop/data structure/data-structure---c/02_TreeStructrue
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,20 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "D:/mingw-gcc/mingw64/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/25804/Desktop/data structure/data-structure---c/build/01_LinearStructure/cmake_install.cmake")
-  include("C:/Users/25804/Desktop/data structure/data-structure---c/build/02_TreeStructrue/cmake_install.cmake")
-
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/25804/Desktop/data structure/data-structure---c/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
