5cbf410a8180656375561a04bb32e8d2fcf33131
YYUCHII <3541838033@qq.com>
2025-07-28 20:51:42 +0800

    二叉树框架搭建

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -39,7 +39,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/25804/Desktop/data structure/data-structure---c/build/01_LinearStructure/cmake_install.cmake")
   include("C:/Users/25804/Desktop/data structure/data-structure---c/build/02_TreeStructrue/cmake_install.cmake")
 
 endif()
