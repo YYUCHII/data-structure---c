208c8c03d841db478d4e245591514d79a4ea92c2
YYUCHII <3541838033@qq.com>
2025-08-06 17:03:16 +0800

    归并排序的实现

--- 04_Sort/04_MergeSort/MergeSort.c
+++ 04_Sort/04_MergeSort/MergeSort.c
@@ -1,2 +1,48 @@
 #include"MergeSort.h"
 
+void Merge(int * table, int left,int mid, int right)
+{
+	int n1 = mid-left;
+	int n2 = right - mid;
+
+	int* arr1 = malloc(sizeof(int)*n1);
+	int * arr2 = malloc(sizeof(int)*n2);
+
+	for(int i = 0 ; i < n1 ; i++) arr1[i] = table[left+i];
+	for(int i = 0 ; i < n2 ; i++) arr2[i] = table[mid+i];
+
+	int i = 0;
+	int j = 0;
+	int k = left;
+
+	while(i < n1 && j < n2)
+	{
+		if(arr1[i] <= arr2[j]) table[k] = arr1[i++];
+		else table[k] = arr2[j++];
+		k++;
+	}
+
+	while(i < n1) table[k++] = arr1[i++];
+	while(j < n2) table[k++] = arr2[j++];
+
+	free(arr1);
+	free(arr2);
+}
+
+void MergeSortTool(int * table, int left, int right)
+{
+	if(right - left <= 1) return ;
+
+	int mid = (right + left) / 2;
+
+	MergeSortTool(table, left, mid);
+	MergeSortTool(table, mid, right);
+
+	Merge(table,left,mid,right);
+}
+
+
+void MergeSort(int * table, int length)
+{
+	MergeSortTool(table, 0, length);
+}
--- 04_Sort/04_MergeSort/MergeSort_main.c
+++ 04_Sort/04_MergeSort/MergeSort_main.c
@@ -2,5 +2,16 @@
 
 int main()
 {
-	
+	int table[5] = {3,2,4,5,1};
+
+	printf("1\n");
+
+	MergeSort(table,5);
+
+	for(int i= 0 ; i < 5 ; i++)
+	{
+		printf("%d\t",table[i]);
+	}
+
+	return 0;
 }
